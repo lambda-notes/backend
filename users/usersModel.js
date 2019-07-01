a5eb330985a3b93af638a02ee7ae0e84b5d7e9bc
Nathan Thomas <nwthomas@me.com>
2019-07-01 12:10:08 -0700

    Build out .findById() knex.js model function

--- users/usersModel.js
+++ users/usersModel.js
@@ -24,7 +24,20 @@ function find() {
 }
 
 function findById(id) {
-  return db('users').where({ id });
+  return db('users as u')
+    .join('accountType as a', 'u.accountType', 'a.id')
+    .join('cohorts as c', 'u.cohortID', 'c.id')
+    .select(
+      'u.id',
+      'u.firstName',
+      'u.lastName',
+      'u.accountType as accountTypeID',
+      'a.accountType',
+      'u.cohortID',
+      'c.cohortName'
+    )
+    .where(`u.id`, id)
+    .first();
 }
 
 function insert(creds) {
