6c7111b8add800cca1a098ca25ea3ebd88c668a4
Nathan Thomas <nwthomas@me.com>
2019-07-01 12:25:05 -0700

    Add DELETE path to userRouter.js

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -124,17 +124,19 @@ router.delete('/:id', async (req, res) => {
     const deletedUser = await Users.remove(req.params.id);
     if (deletedUser) {
       res.status(200).json({
-        message: 'User was deleted successfully.',
-        numDeleted: deletedUser
+        error: false,
+        message: 'User was deleted successfully.'
       });
     } else {
-      res
-        .status(404)
-        .json({ message: 'The user could not be deleted in the database.' });
+      res.status(404).json({
+        error: true,
+        message: 'The user could not be deleted.'
+      });
     }
   } catch (error) {
     res.status(500).json({
-      message: 'There was an error while deleting the user in the database.',
+      error: true,
+      message: 'There was an error while deleting the user.',
       error
     });
   }
