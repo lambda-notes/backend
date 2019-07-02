a05534d522292ca82180e50437043be9c588d174
Henry <40742091+henron1@users.noreply.github.com>
2019-07-02 00:26:16 -0400

    Update passportConfig.js

--- auth/passport/passportConfig.js
+++ auth/passport/passportConfig.js
@@ -40,42 +40,42 @@ module.exports = function(passport_param) {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.GITHUB_CALLBACK_URL || '/auth/github/redirect'
+        callbackURL: process.env.GITHUB_CALLBACK_URL || 'https://lambda-notes-hackathon.netlify.com/auth/github/callback'
 
       },
-      //   function(accessToken, refreshToken, profile, cb) {
-      //     User.findOrCreate({ githubId: profile.id }, function(err, user) {
-      //       return cb(err, user);
-      //     });
-      //   }
-
-      async (accessToken, refreshToken, profile, cb) => {
-        const existingUser = await db('users')
-          .where({
-            email: profile.emails[0].value
-          })
-          .first();
-        if (existingUser) {
-          let accessToken = generateToken.generateToken(existingUser.email);
-          existingUser.token = accessToken;
-          done(null, existingUser); // supplies passport with the user that has authenticated
-        } else {
-          let accessToken = generateToken.generateToken(
-            profile.emails[0].value
-          );
-          await db('users').insert({
-            githubId: profile.id,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            email: profile.emails[0].value,
-            token: accessToken
+        function(accessToken, refreshToken, profile, cb) {
+          User.findOrCreate({ githubId: profile.id }, function(err, user) {
+            return cb(err, user);
           });
-          const user = await db('users')
-            .where({ email: profile.emails[0].value })
-            .first();
-          done(null, user);
         }
-      }
+
+//       async (accessToken, refreshToken, profile, cb) => {
+//         const existingUser = await db('users')
+//           .where({
+//             email: profile.emails[0].value
+//           })
+//           .first();
+//         if (existingUser) {
+//           let accessToken = generateToken.generateToken(existingUser.email);
+//           existingUser.token = accessToken;
+//           done(null, existingUser); // supplies passport with the user that has authenticated
+//         } else {
+//           let accessToken = generateToken.generateToken(
+//             profile.emails[0].value
+//           );
+//           await db('users').insert({
+//             githubId: profile.id,
+//             firstName: profile.name.givenName,
+//             lastName: profile.name.familyName,
+//             email: profile.emails[0].value,
+//             token: accessToken
+//           });
+//           const user = await db('users')
+//             .where({ email: profile.emails[0].value })
+//             .first();
+//           done(null, user);
+//         }
+//       }
     )
   );
 };
