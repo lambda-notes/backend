05269f74d504f3039389f04cce576e3442a850a5
Henry <40742091+henron1@users.noreply.github.com>
2019-07-02 19:14:25 -0400

    Update passportConfig.js

--- auth/passport/passportConfig.js
+++ auth/passport/passportConfig.js
@@ -70,7 +70,7 @@ module.exports = function(passport_param) {
             githubId: profile.id,
             firstName: profile.name,
             lastName: profile.name,
-            accountType: profile.name,
+            accountType: 12345,
             email: profile.email,
             token: accessToken,
             cohortID: 1
